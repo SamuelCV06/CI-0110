ae9799efb2a7a8d88ba677b793dd43f0b1a3410c
SamuelCV06 <samuel.chinchilla@ucr.ac.cr>
2025-05-03 23:11:31 -0600

    Add files via upload

--- Practica 0.1.py
+++ Practica 0.1.py
@@ -3,35 +3,30 @@
 """ Esto no tiene sentido 
     me entienes reealemtne no tiene logica
 	pero bueno """
-"""
-restart = "s"
-while restart == "s":
-     print ("¡Buenos días! \n Por favor, indique su edad:") 
-     edad = float(input ()) 
-     if edad < 6:
-          print ("El precio del tiquete es de $2.00") 
-     elif edad > 10:
-          print ("El precio del tiquete es de $3") 
-     else:
-          print ("El precio del tiquete es de $2,50")
-     print ("quieres reiniciar s/n")
-     restart = input("¿Quieres volver a ejecutar el programa? (s/n): ")
-"""     
-#Variable inicial
-restart = "s"
 
+restart = "s"
 while restart == "s":
      try:
-          edad = int(input("¡Buenos días! \n Por favor, indique su edad:"))
+          print ("¡Buenos días! \n Por favor, indique su edad:") 
+          edad = float(input ()) 
           if edad < 6:
                print ("El precio del tiquete es de $2.00") 
-               restart = input("¿Quieres volver a ejecutar el programa? (s/n): ")
+               print("¿Quieres volver a ejecutar el programa? (s/n): ")
           elif edad > 10:
                print ("El precio del tiquete es de $3")
-               restart = input("¿Quieres volver a ejecutar el programa? (s/n): ")
+               print("¿Quieres volver a ejecutar el programa? (s/n): ")
           else:
                print ("El precio del tiquete es de $2,50")
-               restart = input("¿Quieres volver a ejecutar el programa? (s/n): ")
-          
+               print("¿Quieres volver a ejecutar el programa? (s/n): ")
+          restart = input()
+          if restart == "n" or "no" or "No" or "no":
+               print ("Gracias por usar este programa")
+          input()
      except ValueError:
-          restart = input("Acción no válida \n¿Quieres volver a ejecutar el programa? (s/n): ")
+          print("Número no reconocido")
+          print ("quieres reiniciar s/n")
+          restart = input()
+          if restart == "n" or "no" or "No" or "no":
+               print ("Gracias por usar este programa")
+
+
