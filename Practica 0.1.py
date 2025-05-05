ef68afcded13e4dec18207ed0bd6bde449f57c15
SamuelCV06 <samuel.chinchilla@ucr.ac.cr>
2025-05-04 20:21:41 -0600

    Add files via upload

--- Practica 0.1.py
+++ Practica 0.1.py
@@ -1,11 +1,10 @@
 #Esto es un comentario en Python
-
 """ Esto no tiene sentido 
     me entienes reealemtne no tiene logica
 	pero bueno """
 
 restart = "s"
-while restart == "s":
+while restart == ("s") :
      try:
           print ("¡Buenos días! \n Por favor, indique su edad:") 
           edad = float(input ()) 
@@ -19,14 +18,63 @@ while restart == "s":
                print ("El precio del tiquete es de $2,50")
                print("¿Quieres volver a ejecutar el programa? (s/n): ")
           restart = input()
-          if restart == "n" or "no" or "No" or "no":
+          if restart == "n":
+               try:
+                    print ("Gracias por usar este programa")
+                    break
+                    input()
+               except restart == ("s") or ("si") or ("SI") or ("Si") or ("S"):
+                    restart = "s"
+          elif restart == "no":
+                try:
+                    print ("Gracias por usar este programa")
+                    break
+                    input()
+                except restart == ("s") or ("si") or ("SI") or ("Si") or ("S"):
+                    restart = "s"
+          elif restart == "No":
+               try:
+                    print ("Gracias por usar este programa")
+                    break
+                    input()
+               except restart == ("s") or ("si") or ("SI") or ("Si") or ("S"):
+                    restart = "s"
+          elif restart == "NO":
+               try:
+                    print ("Gracias por usar este programa")
+                    break
+               except restart == ("s") or ("si") or ("SI") or ("Si") or ("S"):
+                    restart = "s"
+                    restart == "s"
+          elif restart == "Si":
+               try: restart = "s"
+               except restart == "n":
+                    restart = "n"
+          elif restart == "si":
+               try: restart = "s"
+               except restart == "n":
+                    restart = "n"
+          elif restart == "SI":
+               try: restart = "s"
+               except restart == "n":
+                    restart = "n"
+          elif restart == "s":
+               try: restart = "s"
+               except restart == "n":
+                    restart = "n"
+          elif restart == "S":
+               try: restart = "s"
+               except restart == "n":
+                    restart = "n"
+          else:
                print ("Gracias por usar este programa")
-          input()
+               break
      except ValueError:
           print("Número no reconocido")
           print ("quieres reiniciar s/n")
           restart = input()
-          if restart == "n" or "no" or "No" or "no":
+          if restart == ("n") or ("no") or ("No") or ("no"):
                print ("Gracias por usar este programa")
+               input()
 
 
