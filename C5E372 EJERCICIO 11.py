0dc2b2d766bca7d817aea592f51376ced7a9cb43
SamuelCV06 <samuel.chinchilla@ucr.ac.cr>
2025-05-09 14:36:51 -0600

    Add files via upload

---
+++ C5E372 EJERCICIO 11.py
@@ -0,0 +1,40 @@
+"""
+Imprimir en pantalla las tablas de multiplicar (incluye valores multiplicados desde el 1 hasta el 12),
+ según el reango de las tablas especificado por el usuario. cada tabla debe tener un encabezado y en 
+  cada fila se imprime el resultado de una multiplicación. Por ejemplo: 1 x 2 = 2
+"""
+""" 
+tabla_1 <----- solicitar al usuario
+tabla_2 <----- solicitar al usario
+numero <---- tabla_1
+mult <= 0
+multi_maximo <= 12
+multi_minimo <= 0
+for numero in range(numero, tabla_2, multi_maximo + 1)
+multi *= numero
+if multi <-- 0
+print tabla range(numero, tabla_2, multi_maximo)
+multi += 1
+print (multi) 
+print ("son los valores de las tablas selecionadas)
+"""
+
+#PEDIR LOS NUMEROS PARA LAS TABLAS
+
+tabla_1 = int(input("Selecciones el numero menor del rango deseado:"))
+tabla_2 = int(input("Selecciones el numero mayor del rango deseado:"))
+numero = tabla_1
+mult = 0
+Mult_max = 12
+Mult_min = 0
+for numero in range(numero, tabla_2, Mult_max + 1):
+    if mult == 0: 
+        print("Tabla", range(numero, tabla_2, Mult_max))
+        mult *= numero
+        print(mult)
+
+print("son los valores de las tablas selecionadas")
+
+
+
+           
