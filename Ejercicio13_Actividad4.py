5d8ec5c24bf32cc6d84f46fb73aa7c5e0eb1adb0
SamuelCV06 <samuel.chinchilla@ucr.ac.cr>
2025-05-26 13:11:48 -0600

    Acti 14 v 2

--- Ejercicio13_Actividad4.py
+++ Ejercicio13_Actividad4.py
@@ -4,27 +4,30 @@ CI-0110 Introducción a la computación
 C5E372 Samuel Chinchilla Vásquez 
 Proposito:  
 Crear una lista con 15 números aleatorios entre 1 y 100, para luego pedir al usuario un número:
-si el número indicado por el usuario está en la lista, el usuario “ganó” el juego y se le debe indicar la posición
-en que se encontró; si no adivina el número después de tres intentos, se le muestran todos los valores
-contenidos en la lista.
+si el número indicado por el usuario está en la lista, el usuario “ganó” 
+el juego y se le debe indicar la posición en que se encontró; si no adivina el número 
+después de tres intentos, se le muestran todos los valores contenidos en la lista.
 
 Algoritmo: 
 1. lista <---- []
 2. i <-- 1
 3. while i <= 15 
 3.1 num aleatorio <--- generara número aleatorio(1, 100)
-3.2 lista <--- añadir num aleatorio
+3.2 lista <--- añadir num aleatorios
 3.3 i = i + 1
-4. while p < 3 + 1 
-5. numero_jugador <----- pedir al usuario
-6. if numero_jugador <= numero en lista then
-6.1. print <--- "Felicidades usted gano, su numero se ubicaba en la posicion  " 
-6.1.1 break
-7. else 
-7.1 print <-- "Intentelo de nuevo"
-7.1.1 p <= p + 1
-8. else
-8.1 print <-- "Usted perdio, estos eran los numeros correctos", lista  
+4. p <-- 1
+5. while p <= 3 
+6. numero_jugador <----- pedir al usuario
+7. if numero_jugador in lista then
+7.1. x <-- posición en la lista
+7.1.1 print <--- "Felicidades usted ha ganado con el número que se encuentra en la posición",x,"de la lista." 
+7.1.1.1 break
+8 else
+8.1 if p < 3 then
+8.1.1 print <-- " Su número no esta en la lista, intentelo de nuevo "
+8.1.1.1 p <= p + 1
+9. else
+9.1 print <-- " Usted perdió, los números correctos son los siguientes", lista "  
 """
 #Importar libreria ramdom
 import random as rn
@@ -38,20 +41,18 @@ for i in range(15):
     N_A = rn.randint(1, 100)
     lista.append (N_A)
     i *= 1
-#Pedimos el numero al usuario
-#Esta lista es solo para comprobar que funciona con los numeros correctos:
-print(lista)
-while p <= 31:
+#Pedimos el número para jugar al usuario 
+#print (lista) # <----- Quitar el # del print si se complica el adivinar el número 
+while p <= 3:
     n_j = int(input("Digite un número del 1 al 100 para ganar: \n"))
     if n_j in lista:
         x = lista.index(n_j)
-        print ("Felicidades usted ha ganado con el numero en la posición", x)
+        print (" Felicidades usted ha ganado con el número \n que se encuentra en la posición",x, "de la lista.")
         break
-    elif p <3 : 
-        print("Intentelo de nuevo")
+    #Si el número es incorrecto se le dan 2 oportunidades más al jugador
+    elif p < 3 : 
+        print(" Su número no esta en la lista, intentelo de nuevo")
         p +=1
     else: 
-       print("Usted perdio, los nuemeros correctos eran", lista)
+       print("Usted perdió, los números correctos son los siguientes\n", lista)
        break
-else: 
-    print("Usted perdio, los nuemeros correctos eran", lista)
